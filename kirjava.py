1dce3fa724d54ae6c5842b9fe2645eb1d4407400
Sam Ireland <sam.ireland.uk@gmail.com>
2019-03-08 21:01:50 +0000

    Basic execute function

--- kirjava.py
+++ kirjava.py
@@ -1,2 +1,12 @@
 __version__ = "0.1.0"
 __author__ = "Sam Ireland"
+
+import requests
+import json
+
+def execute(url, message, method="POST", headers=None):
+    head = {"Accept": "application/json", "Content-Type": "application/json"}
+    if headers: head = {**head, **headers}
+    data = {"query": message}
+    resp = requests.request(method, url, headers=head, data=json.dumps(data))
+    return resp.json()
