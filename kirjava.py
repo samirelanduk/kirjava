f0841f54ccb6e1fcffebf0e0f6f02cfd6cca03a8
Sam Ireland <sam.ireland.uk@gmail.com>
2019-03-23 23:10:10 +0000

    Remake as Client object

--- kirjava.py
+++ kirjava.py
@@ -4,9 +4,28 @@ __author__ = "Sam Ireland"
 import requests
 import json
 
-def execute(url, message, method="POST", headers=None):
-    head = {"Accept": "application/json", "Content-Type": "application/json"}
-    if headers: head = {**head, **headers}
-    data = {"query": message}
-    resp = requests.request(method, url, headers=head, data=json.dumps(data))
-    return resp.json()
+class Client:
+
+    def __init__(self, url):
+        self._url = url
+        self._headers = {
+         "Accept": "application/json", "Content-Type": "application/json"
+        }
+
+
+    @property
+    def url(self):
+        return self._url
+
+
+    @property
+    def headers(self):
+        return self._headers
+
+
+    def execute(self, message, method="POST"):
+        data = {"query": message}
+        resp = requests.request(
+         method, self._url, headers=self._headers, data=json.dumps(data)
+        )
+        return resp.json()
