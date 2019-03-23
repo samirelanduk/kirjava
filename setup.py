024e78c3a9cfae6ced94a5f216ee83990c31a4b4
Sam Ireland <sam.ireland.uk@gmail.com>
2019-03-23 23:43:39 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+ name="kirjava",
+ version="0.1.0",
+ description="A Python GraphQL client.",
+ url="https://kirjava.samireland.com",
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Developers",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: Internet",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.6",
+  "Programming Language :: Python :: 3.7",
+ ],
+ keywords="GraphQL",
+ py_modules=["kirjava"],
+ install_requires=["requests"]
+)
