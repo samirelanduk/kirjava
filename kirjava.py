bb0a3f9f49b6443c5d53b56562d69be9df1dcfb5
Sam Ireland <sam.ireland.uk@gmail.com>
2019-03-23 23:24:11 +0000

    Add docstrings

--- kirjava.py
+++ kirjava.py
@@ -5,6 +5,12 @@ import requests
 import json
 
 class Client:
+    """A GraphQL client. This is the object which sends requests to the GraphQL
+    server.
+
+    The URL that serves the GraphQL content is given on creating the Client.
+
+    :param str url: The URL of the GraphQL server to interact with."""
 
     def __init__(self, url):
         self._url = url
@@ -15,15 +21,31 @@ class Client:
 
     @property
     def url(self):
+        """The URL of the GraphQL server to interact with.
+
+        :rtype: ``str``"""
+
         return self._url
 
 
     @property
     def headers(self):
+        """The HTTP headers that will be sent with every request.
+
+        :rtype: ``dict``"""
+
         return self._headers
 
 
     def execute(self, message, method="POST", variables=None):
+        """Sends a request to the GraphQL server.
+
+        :param str message: The query to make.
+        :param str method: By default, POST requests are sent, but this can be\
+        overriden here.
+        :param dict variables: Any GraphQL variables can be passed here.
+        :rtype: ``dict``"""
+
         data = {"query": message}
         if variables: data["variables"] = variables
         resp = requests.request(
