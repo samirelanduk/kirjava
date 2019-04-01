21775e8009f80eb40c0d5788589f39b554e94264
Sam Ireland <sam.ireland.uk@gmail.com>
2019-04-01 21:51:48 +0100

    Start 0.1.2

--- kirjava.py
+++ kirjava.py
@@ -1,6 +1,6 @@
 """The kirjava library."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Sam Ireland"
 
 import requests
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="kirjava",
- version="0.1.1",
+ version="0.1.2",
  description="A Python GraphQL client.",
  url="https://kirjava.samireland.com",
  author="Sam Ireland",
