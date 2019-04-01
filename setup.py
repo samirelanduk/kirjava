a2886d625cfbb765ba2763f8303719306b26c0ac
Sam Ireland <sam.ireland.uk@gmail.com>
2019-04-01 21:58:03 +0100

    Add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.rst") as f:
+    long_description = f.read()
+
 setup(
  name="kirjava",
  version="0.1.2",
  description="A Python GraphQL client.",
+ long_description=long_description,
+ long_description_content_type="test/x-rst",
  url="https://kirjava.samireland.com",
  author="Sam Ireland",
  author_email="mail@samireland.com",
