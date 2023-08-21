2b69f6953d06a8bac28dc5811baadb4081107c0d
Sam Ireland <mail@samireland.com>
2023-08-21 01:50:43 +0100

    Add retry logic

--- kirjava/client.py
+++ kirjava/client.py
@@ -1,8 +1,8 @@
 """Contains the Client class itself."""
 
-import requests
 import json
-
+import requests
+import time
 from .utilities import files_to_map, get_files_from_variables, create_response_error_message, pack_files
 
 class Client:
@@ -54,13 +54,15 @@ class Client:
         return tuple(self._history)
 
 
-    def execute(self, message, method="POST", variables=None):
+    def execute(self, message, method="POST", variables=None, retries=0, retry_statuses=None):
         """Sends a request to the GraphQL server.
 
         :param str message: The query to make.
         :param str method: By default, POST requests are sent, but this can be\
         overriden here.
         :param dict variables: Any GraphQL variables can be passed here.
+        :param int retries: The number of times to retry on failure.
+        :param list retry_statuses: The HTTP statuses to retry on.
         :rtype: ``dict``"""
 
         headers = {key: value for key, value in self._headers.items()}
@@ -70,12 +72,14 @@ class Client:
             del headers["Content-Type"]
             data = {"operations": operation, "map": json.dumps(files_to_map(files))}
             files = pack_files(files)
-            response = self.session.request(
-                method, self._url, files=files, data=data, headers=headers
+            response = self.request_with_retries(
+                operation=data, headers=headers, method=method,
+                retries=retries, retry_statuses=retry_statuses, files=files
             )
         else:
-            response = self.session.request(
-                method, self._url, headers=headers, data=operation
+            response = self.request_with_retries(
+                operation=operation, headers=headers, method=method,
+                retries=retries, retry_statuses=retry_statuses
             )
         try:
             result = response.json()
@@ -86,5 +90,30 @@ class Client:
         ))
         return result
 
-       
 
+    def request_with_retries(self, operation, headers, files=None, method="POST", retries=0, retry_statuses=None):
+        """Sends a GraphQL request, retrying if necessary the specified number
+        of times.
+        
+        :param str operation: The GraphQL operation to send.
+        :param dict headers: The HTTP headers to send.
+        :param dict files: The files to send.
+        :param str method: The HTTP method to use.
+        :param int retries: The number of times to retry.
+        :param list retry_statuses: The HTTP statuses to retry on.
+        :rtype: ``requests.Response``"""
+
+        attempts = 0
+        while True:
+            try:
+                response = self.session.request(
+                    method, self._url, headers=headers, data=operation, files=files
+                )
+                if retry_statuses and response.status_code in retry_statuses:
+                    raise Exception(f"Status code {response.status_code}")
+                return response
+            except Exception as e:
+                attempts += 1
+                if attempts > retries: raise e
+                time.sleep(2**attempts)
+                continue
--- tests/unit/test_clients.py
+++ tests/unit/test_clients.py
@@ -55,10 +55,12 @@ class ClientExecutionTests(TestCase):
         self.patch2 = patch("kirjava.client.files_to_map")
         self.patch3 = patch("kirjava.client.pack_files")
         self.patch4 = patch("kirjava.client.create_response_error_message")
+        self.patch5 = patch("kirjava.client.Client.request_with_retries")
         self.mock_files = self.patch1.start()
         self.mock_map = self.patch2.start()
         self.mock_pack = self.patch3.start()
         self.mock_error = self.patch4.start()
+        self.mock_request = self.patch5.start()
         self.mock_map.return_value = {"0": ["MAP"]}
         self.mock_pack.return_value = {"0": ["packed"]}
         self.mock_files.return_value = (None, None)
@@ -69,6 +71,7 @@ class ClientExecutionTests(TestCase):
         self.patch2.stop()
         self.patch3.stop()
         self.patch4.stop()
+        self.patch5.stop()
 
 
     def test_can_send_query(self):
@@ -76,14 +79,30 @@ class ClientExecutionTests(TestCase):
         client.session = Mock()
         result = client.execute("MESSAGE")
         self.mock_files.assert_called_with(None)
-        client.session.request.assert_called_with(
-            "POST", "http://url", headers=client._headers,
-            data='{"variables": null, "query": "MESSAGE"}'
+        self.mock_request.assert_called_with(
+            operation='{"variables": null, "query": "MESSAGE"}',
+            headers=client._headers, method="POST", retries=0, retry_statuses=None
         )
-        self.assertEqual(result, client.session.request.return_value.json.return_value)
+        self.assertEqual(result, self.mock_request.return_value.json.return_value)
         self.assertEqual(client._history, [(
             {"query": "MESSAGE", "variables": {}},
-            client.session.request.return_value.json.return_value
+            self.mock_request.return_value.json.return_value
+        )])
+    
+
+    def test_can_send_query_with_retries(self):
+        client = Client("http://url")
+        client.session = Mock()
+        result = client.execute("MESSAGE", retries=3, retry_statuses=[1, 2])
+        self.mock_files.assert_called_with(None)
+        self.mock_request.assert_called_with(
+            operation='{"variables": null, "query": "MESSAGE"}',
+            headers=client._headers, method="POST", retries=3, retry_statuses=[1, 2]
+        )
+        self.assertEqual(result, self.mock_request.return_value.json.return_value)
+        self.assertEqual(client._history, [(
+            {"query": "MESSAGE", "variables": {}},
+            self.mock_request.return_value.json.return_value
         )])
 
 
@@ -91,11 +110,11 @@ class ClientExecutionTests(TestCase):
         client = Client("http://url")
         client.session = Mock()
         result = client.execute("MESSAGE", method="GET")
-        client.session.request.assert_called_with(
-            "GET", "http://url", headers=client._headers,
-            data='{"variables": null, "query": "MESSAGE"}'
+        self.mock_request.assert_called_with(
+            operation='{"variables": null, "query": "MESSAGE"}',
+            headers=client._headers, method="GET", retries=0, retry_statuses=None
         )
-        self.assertEqual(result, client.session.request.return_value.json.return_value)
+        self.assertEqual(result, self.mock_request.return_value.json.return_value)
 
 
     def test_can_send_query_with_variables(self):
@@ -104,13 +123,14 @@ class ClientExecutionTests(TestCase):
         self.mock_files.return_value = ({"S": "T"}, None)
         result = client.execute("MESSAGE", variables={"S": "T"})
         self.mock_files.assert_called_with({"S": "T"})
-        client.session.request.assert_called_with(
-            "POST", "http://url", headers=client._headers, data='{"variables": {"S": "T"}, "query": "MESSAGE"}'
+        self.mock_request.assert_called_with(
+            operation='{"variables": {"S": "T"}, "query": "MESSAGE"}',
+            headers=client._headers, method="POST", retries=0, retry_statuses=None
         )
-        self.assertEqual(result, client.session.request.return_value.json.return_value)
+        self.assertEqual(result, self.mock_request.return_value.json.return_value)
         self.assertEqual(client._history, [(
             {"query": "MESSAGE", "variables": {"S": "T"}},
-            client.session.request.return_value.json.return_value
+            self.mock_request.return_value.json.return_value
         )])
     
 
@@ -125,30 +145,128 @@ class ClientExecutionTests(TestCase):
         self.mock_files.return_value = ({"S": "T"}, {"file1": file1, "file2": file2})
         result = client.execute("MESSAGE", variables={"S": "T"})
         self.mock_files.assert_called_with({"S": "T"})
-        client.session.request.assert_called_with(
-            "POST", "http://url",
+        self.mock_request.assert_called_with(
+            method="POST", retries=0, retry_statuses=None,
             files={"0": ["packed"]}, 
-            data={
+            operation={
                 "operations": '{"variables": {"S": "T"}, "query": "MESSAGE"}',
                 "map": '{"0": ["MAP"]}'
             },
             headers={'Accept': "application/json"},
         )
-        self.assertEqual(result, client.session.request.return_value.json.return_value)
+        self.assertEqual(result, self.mock_request.return_value.json.return_value)
         self.assertEqual(client._history, [(
             {"query": "MESSAGE", "variables": {"S": "T"}},
-            client.session.request.return_value.json.return_value
+            self.mock_request.return_value.json.return_value
         )])
     
 
     def test_can_handle_non_json_response(self):
         client = Client("http://url")
         client.session = Mock()
-        client.session.request.return_value.json.side_effect = json.decoder.JSONDecodeError("", "", 0)
+        self.mock_request.return_value.json.side_effect = json.decoder.JSONDecodeError("", "", 0)
         with self.assertRaises(ValueError) as e:
             client.execute("MESSAGE")
-        self.mock_error.assert_called_with(client.session.request.return_value)
+        self.mock_error.assert_called_with(self.mock_request.return_value)
         self.assertEqual(
             str(e.exception),
             str(self.mock_error.return_value)
         )
+
+
+
+class ClientRetryTests(TestCase):
+
+    def test_simple_request_without_retries(self):
+        client = Client("http://url")
+        client.session = Mock()
+        client.session.request.return_value.status_code = 500
+        resp = client.request_with_retries(
+            operation="operation", headers="headers", method="method", files="files"
+        )
+        self.assertEqual(resp, client.session.request.return_value)
+        client.session.request.assert_called_with(
+            "method", "http://url", data="operation", headers="headers", files="files"
+        )
+    
+
+    def test_zero_retry_allows_failure(self):
+        client = Client("http://url")
+        client.session = Mock()
+        client.session.request.side_effect = Exception()
+        with self.assertRaises(Exception):
+            client.request_with_retries(
+                operation="operation", headers="headers", method="method", files="files"
+            )
+        self.assertEqual(client.session.request.call_count, 1)
+    
+
+    @patch("time.sleep")
+    def test_can_retry(self, mock_sleep):
+        client = Client("http://url")
+        client.session = Mock()
+        client.session.request.side_effect = [Exception, Exception, Exception, "RESP"]
+        resp = client.request_with_retries(
+            operation="operation", headers="headers", method="method", files="files", retries=5
+        )
+        self.assertEqual(resp, "RESP")
+        mock_sleep.assert_any_call(2)
+        mock_sleep.assert_any_call(4)
+        mock_sleep.assert_any_call(8)
+        self.assertEqual(client.session.request.call_count, 4)
+    
+
+    @patch("time.sleep")
+    def test_can_retry_to_limit(self, mock_sleep):
+        client = Client("http://url")
+        client.session = Mock()
+        client.session.request.side_effect = [Exception, Exception, Exception, "RESP"]
+        with self.assertRaises(Exception):
+            client.request_with_retries(
+                operation="operation", headers="headers", method="method", files="files", retries=2
+            )
+        mock_sleep.assert_any_call(2)
+        mock_sleep.assert_any_call(4)
+        self.assertEqual(client.session.request.call_count, 3)
+    
+
+    def test_simple_request_without_retries(self):
+        client = Client("http://url")
+        client.session = Mock()
+        resp = client.request_with_retries(
+            operation="operation", headers="headers", method="method", files="files"
+        )
+        self.assertEqual(resp, client.session.request.return_value)
+        client.session.request.assert_called_with(
+            "method", "http://url", data="operation", headers="headers", files="files"
+        )
+    
+
+    def test_can_fail_on_status_code(self):
+        client = Client("http://url")
+        client.session = Mock()
+        client.session.request.return_value.status_code = 500
+        with self.assertRaises(Exception):
+            client.request_with_retries(
+                operation="operation", headers="headers", method="method", files="files", retry_statuses=[500]
+            )
+        client.session.request.assert_called_with(
+            "method", "http://url", data="operation", headers="headers", files="files"
+        )
+    
+
+    @patch("time.sleep")
+    def test_can_retry_on_status_code(self, mock_sleep):
+        client = Client("http://url")
+        client.session = Mock()
+        client.session.request.return_value.status_code = 500
+        with self.assertRaises(Exception):
+            client.request_with_retries(
+                operation="operation", headers="headers", method="method", files="files", retry_statuses=[500], retries=5
+            )
+        mock_sleep.assert_any_call(2)
+        mock_sleep.assert_any_call(4)
+        mock_sleep.assert_any_call(8)
+        mock_sleep.assert_any_call(16)
+        mock_sleep.assert_any_call(32)
+        self.assertEqual(client.session.request.call_count, 6)
